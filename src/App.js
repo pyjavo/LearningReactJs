4ebb477d15bf4b5b49d759432429bad1e90d4007
Jeyson Lamb <lambjeyson@gmail.com>
2024-09-21 09:50:12 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Card from "./components/card"; // Importamos el componente de la tarjeta
+import $imag from "./Imagenes/Law.png";
+import $imag1 from "./Imagenes/DonFlamingo.png";
+import $imag2 from "./Imagenes/Corazon.png";
 
 function App() {
+  // Creamos un array de objetos con los nombres y las imágenes
+  const characters = [
+    { name: "Trafalgar Law", image: $imag,  status: "Estado: Se esta recuperando" },
+    { name: "Donquixote Doflamingo", image: $imag1, status: "Estado: Derrotado(Preso)"  },
+    { name: "Donquixote Rosinante", image: $imag2, status: "Estado: Fallecio" },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card-container">
+      {characters.map((character, index) => (
+        <Card key={index} 
+        name={character.name} 
+        image={character.image}  
+        status={character.status} />
+      ))}
     </div>
   );
 }
